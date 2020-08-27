c4fa5024040379a0727082bcbc6c792688193439
stonebank <35108449>
2020-08-27 17:32:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import time
+
+from PIL import ImageGrab
+from selenium import webdriver
+
+browser = webdriver.Safari()
+browser.maximize_window()
+browser.get("https://orteil.dashnet.org/cookieclicker/")
+time.sleep(2)
+
+while True:
+    browser.find_element_by_id("bigCookie").click()
+    battery = int(subprocess.getoutput("pmset -g batt").split("%")[0].split(" ")[6].split("\t")[1])
+    if battery <= 24:
+        snapchot = ImageGrab.grab()
+        snapchot.save("/Users/hk/Desktop/screenshot.png")
+        os.system("shutdown -h now")
+
