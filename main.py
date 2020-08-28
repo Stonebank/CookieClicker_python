1ea922ddb194922b615852829f66e0717594be0b
stonebank <35108449>
2020-08-28 09:29:34 +0200

    final commit

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ time.sleep(2)
 while not shutting_down:
     browser.find_element_by_id("bigCookie").click()
     battery = int(subprocess.getoutput("pmset -g batt").split("%")[0].split(" ")[6].split("\t")[1])
-    if battery <= 60:
+    if battery <= 10:
         screenshot = ImageGrab.grab()
         screenshot.save("/Users/hk/Desktop/screenshot.png")
         subprocess.run(["say", "Bomben er blevet plantet, den vil springe om 40 sekunder."])
-        subprocess.call(['osascript', '-e',
-                         'tell app "System Events" to shut down'])
+        subprocess.call(['osascript', '-e', 'tell app "System Events" to shut down'])
         shutting_down = True
 
